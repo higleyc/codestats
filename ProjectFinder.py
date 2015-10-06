97843cf84f5ddc64c9b1975dc5b5f6c5f8588bd9
Chris Higley <higleyc@gmail.com>
2015-10-05 21:40:09 -0400

    Added basic project finder functionality

--- ProjectFinder.py
+++ ProjectFinder.py
@@ -1,2 +1,24 @@
+from github import Github
+from Project import Project
+
+LANGUAGES_TO_ANALYZE = ["C", "C++"]
+
 class ProjectFinder:
-    pass
+    def __init__(self):
+        self.github = Github()
+        self.results = None
+    
+    def get_projects(self):
+        if not self.results:
+            self.results = self.github.search_repositories("code")
+            self.page_at = 0
+        
+        repos = self.results.get_page(self.page_at)
+        self.page_at += 1
+        
+        results = []
+        for repo in repos:
+            if repo.language in LANGUAGES_TO_ANALYZE:
+                results.append(Project(repo))
+        
+        return results
